f28cf6ce0f36c4274d913e2265d6b58fddc0fde0
Daviscrack503 <eg0078032024@unab.edu.sv>
2025-03-13 13:45:28 -0600

    Bucles semana 5

---
+++ bucles semana5/Ejemplosbucles.vb
@@ -0,0 +1,61 @@
+﻿Public Class Ejemplosbucles
+    Public Function buclefor(ByVal limite As Integer)
+        For i = 1 To limite
+            Console.WriteLine(i)
+        Next
+
+    End Function
+
+    Public Function bucleforeach()
+        Dim miArray As Integer() = {10, 20, 30, 40, 50}
+        Dim elemento As Integer
+
+
+        For Each elemento In miArray
+            Debug.Print("Valor: " & elemento)
+        Next
+
+    End Function
+
+    Public Function bucleWileLoop()
+        Dim x As Integer
+        x = 1
+        Do While x <= 5
+            Debug.Print("Valor de x: " & x)
+            x = x + 1
+        Loop
+
+
+    End Function
+
+
+    Public Function UnitlLoop()
+        Dim y As Integer
+        y = 1
+        Do Until y > 5
+            Debug.Print("Valor de y: " & y)
+            y = y + 1
+        Loop
+
+
+    End Function
+
+    Public Function DoloopWhile()
+        Dim z As Integer
+        z = 1
+        Do
+            Debug.Print("Valor de z: " & z)
+            z = z + 1
+        Loop While z <= 5
+    End Function
+
+    Public Function DoLoopUnitl()
+        Dim w As Integer
+        w = 1
+        Do
+            Debug.Print("Valor de w: " & w)
+            w = w + 1
+        Loop Until w > 5
+    End Function
+
+End Class
---
+++ bucles semana5/Program.vb
@@ -0,0 +1,30 @@
+Imports System
+Imports Ejemplosbucles
+Module Program
+    Sub Main(args As String())
+        Dim bcfor As New Ejemplosbucles()
+        bcfor.buclefor(5)
+
+        Dim bcforeach As New Ejemplosbucles()
+        bcforeach.bucleforeach()
+
+
+        Dim bcwhile As New Ejemplosbucles()
+        bcwhile.bucleWileLoop()
+
+
+        Dim bcuntil As New Ejemplosbucles()
+        bcuntil.UnitlLoop()
+
+        Dim bcdoWhile As New Ejemplosbucles()
+        bcdoWhile.DoloopWhile()
+
+        Dim bcdoUntil As New Ejemplosbucles()
+        bcdoUntil.DoLoopUnitl()
+
+
+    End Sub
+
+
+
+End Module
